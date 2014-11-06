a512d9b3f2fc7c50f2345f52e03d2d97e46673fb
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-11-06 09:33:34 +0100

    Adds configurable import to init

---
+++ init/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var fs = require('fs')
+  , mongojs = require('mongojs')
+  , config = require('../config')
+  , DB = mongojs(config.DB)
+  , dataList = [
+    {
+      "db":DB.collection('municipality'),
+      "file": "init/data/municipalities.json"
+    },
+    {
+      "db":DB.collection('departments'),
+      "file": "init/data/departments.json"
+    }
+  ]
+  ;
+
+function getData(opts, callback){
+  fs.readFile(opts.file, function(err, data){
+    if(err){
+      return callback(err, null);
+    } else {
+      opts.data = JSON.parse(data.toString());
+      return callback(null, opts);
+    }
+  })
+}
+
+function addDocument(db, post){
+  db.save(post, function (err, success) {
+    if(err){
+      console.error(err);
+    } else {
+      console.log(success);
+    }
+  });
+}
+
+function addDocuments(err, opts){
+  if(err){
+    console.error(err)
+  } else {
+    opts.db.drop();
+    opts.data.forEach(function(document){
+      addDocument(opts.db, document);
+    });
+  }
+}
+
+//Imports all init data
+dataList.forEach(function(opts){
+  getData(opts, addDocuments)
+});
