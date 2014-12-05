603c86805ea7078982ec4905651d3737d93691b0
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-12-05 08:17:31 +0100

    Adds description of routes

--- handlers/public.js
+++ handlers/public.js
@@ -1,7 +1,18 @@
 'use strict';
 
+var message = {
+      message: "Hello, I'm your API",
+      departments: "/departments",
+      department: "/department/<departmentId>",
+      journals: "/journals",
+      journal: "/journal/<journalId>",
+      municipalities: "/municipalities",
+      municipality: "municipality/<municipalityId>"
+    }
+  ;
+
 function getPublicResponse(request, reply) {
-  reply({"message": "Hello, i'm your API"});
+  reply(message);
 }
 
 module.exports.getPublicResponse = getPublicResponse;
