4a62f006d71a66d220f336c05bd61e57c4af1ee6
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-01-13 09:49:17 +0100

    Changes code style

--- handlers/public.js
+++ handlers/public.js
@@ -1,24 +1,23 @@
 'use strict';
 
 var message = {
-      message: "Hello, I'm your API",
-      departments: "/departments",
-      department: "/department/<departmentId>",
-      journals: "/journals",
-      journalsByDateDistinct: "/journals/date/distinct",
-      journalsByDate: "/journals/date/<yyyymmdd>",
-      journalsByDepartment: "/journals/department/<aes192(departmentName)>[?date=<yyyymmdd>]",
-      journalsByDepartmentDistinct: "/journals/department/distinct",
-      journalsCollection: "/journals/collection/<saSeknr>",
-      journalsLatest: "/journals/latest",
-      journal: "/journal/<jpSeknr>",
-      municipalities: "/municipalities",
-      municipality: "/municipality/<municipalityId>",
-      recruitments: "/recruitments",
-      recruitment: "/recruitment/<recruitmentId>",
-        quote: "Thank you for your cooperation"
-    }
-  ;
+  message: 'Hello, I\'m your API',
+  departments: '/departments',
+  department: '/department/<departmentId>',
+  journals: '/journals',
+  journalsByDateDistinct: '/journals/date/distinct',
+  journalsByDate: '/journals/date/<yyyymmdd>',
+  journalsByDepartment: '/journals/department/<aes192(departmentName)>[?date=<yyyymmdd>]',
+  journalsByDepartmentDistinct: '/journals/department/distinct',
+  journalsCollection: '/journals/collection/<saSeknr>',
+  journalsLatest: '/journals/latest',
+  journal: '/journal/<jpSeknr>',
+  municipalities: '/municipalities',
+  municipality: '/municipality/<municipalityId>',
+  recruitments: '/recruitments',
+  recruitment: '/recruitment/<recruitmentId>',
+  quote: 'Thank you for your cooperation'
+};
 
 function getPublicResponse(request, reply) {
   reply(message);
