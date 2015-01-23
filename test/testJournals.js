aafffb1266a760001e551c466ca8634db459cbb0
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-01-23 08:36:01 +0100

    Changes to please jscs

--- test/testJournals.js
+++ test/testJournals.js
@@ -7,18 +7,18 @@ var config = require('../config');
 
 request = request('http://localhost:' + config.SERVER_PORT);
 
-describe('Server journals', function () {
+describe('Server journals', function() {
 
-  before(function () {
+  before(function() {
     server.start();
   });
 
-  after(function () {
+  after(function() {
     server.stop();
   });
 
-  describe('GET /journals', function(){
-    it('respond with json', function(done){
+  describe('GET /journals', function() {
+    it('respond with json', function(done) {
       request
         .get('/journals')
         .set('Accept', 'application/json')
@@ -27,8 +27,8 @@ describe('Server journals', function () {
     });
   });
 
-  describe('GET /journals/date/distinct', function(){
-    it('respond with json', function(done){
+  describe('GET /journals/date/distinct', function() {
+    it('respond with json', function(done) {
       request
         .get('/journals/date/distinct')
         .set('Accept', 'application/json')
@@ -37,8 +37,8 @@ describe('Server journals', function () {
     });
   });
 
-  describe('GET /journals/date/20141016', function(){
-    it('respond with json', function(done){
+  describe('GET /journals/date/20141016', function() {
+    it('respond with json', function(done) {
       request
         .get('/journals/date/20141016')
         .set('Accept', 'application/json')
@@ -47,8 +47,8 @@ describe('Server journals', function () {
     });
   });
 
-  describe('GET /journals/department/distinct', function(){
-    it('respond with json', function(done){
+  describe('GET /journals/department/distinct', function() {
+    it('respond with json', function(done) {
       request
         .get('/journals/department/distinct')
         .set('Accept', 'application/json')
@@ -57,19 +57,19 @@ describe('Server journals', function () {
     });
   });
 
-  describe('GET /journals/department/Seksjon for kvalitet og utvikling', function(){
-    it('respond with json', function(done){
+  describe('GET /journals/department/Seksjon for kvalitet og utvikling', function() {
+    it('respond with json', function(done) {
       var department = cipher.encrypt('Seksjon for kvalitet og utvikling');
       request
-        .get('/journals/department/'+department)
+        .get('/journals/department/' + department)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200, done);
     });
   });
 
-  describe('GET /journals/department/Seksjon for kvalitet og utvikling?date=20141016', function(){
-    it('respond with json', function(done){
+  describe('GET /journals/department/Seksjon for kvalitet og utvikling?date=20141016', function() {
+    it('respond with json', function(done) {
       var department = cipher.encrypt('Seksjon for kvalitet og utvikling');
       request
         .get('/journals/department/' + department + '?date=20141016')
@@ -79,8 +79,8 @@ describe('Server journals', function () {
     });
   });
 
-  describe('GET /journals/collection/65', function(){
-    it('respond with json', function(done){
+  describe('GET /journals/collection/65', function() {
+    it('respond with json', function(done) {
       request
         .get('/journals/collection/65')
         .set('Accept', 'application/json')
@@ -89,8 +89,8 @@ describe('Server journals', function () {
     });
   });
 
-  describe('GET /journals/latest', function(){
-    it('respond with json', function(done){
+  describe('GET /journals/latest', function() {
+    it('respond with json', function(done) {
       request
         .get('/journals/latest')
         .set('Accept', 'application/json')
@@ -99,8 +99,8 @@ describe('Server journals', function () {
     });
   });
 
-  describe('GET /journal/211', function(){
-    it('respond with json', function(done){
+  describe('GET /journal/211', function() {
+    it('respond with json', function(done) {
       request
         .get('/journal/211')
         .set('Accept', 'application/json')
