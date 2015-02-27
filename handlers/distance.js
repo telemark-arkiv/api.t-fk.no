5748fc5ce17ca2e7e34a5bfaa75f0ba81b99b262
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-02-27 19:15:52 +0100

    Adds distance handler

---
+++ handlers/distance.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var distance = require('google-distance');
+
+function getDistance(request, reply) {
+
+  distance.get({
+      origin: request.params.origin,
+      destination: request.params.destination
+    },
+    function(err, data) {
+      if (err) {
+        reply(err);
+      } else {
+        reply(data);
+      }
+    });
+
+}
+
+module.exports = getDistance;
