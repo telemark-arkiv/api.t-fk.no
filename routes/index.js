6b8d8c3a286dd47215f0e7715048668b9eb076aa
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-02-27 19:18:59 +0100

    Adds distance route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var municipalityRoutes = require('./municipalities');
 var departmentRoutes = require('./departments');
 var journalRoutes = require('./journals');
 var recruitmentRoutes = require('./recruitments');
+var distanceRoutes = require('./distance');
 var routes = [];
 
 routes = routes.concat(publicRoutes);
@@ -12,5 +13,6 @@ routes = routes.concat(municipalityRoutes);
 routes = routes.concat(departmentRoutes);
 routes = routes.concat(journalRoutes);
 routes = routes.concat(recruitmentRoutes);
+routes = routes.concat(distanceRoutes);
 
 module.exports = routes;
