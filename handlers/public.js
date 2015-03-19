6f2959cdde10ada66ca16807e202557363c5f1ff
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-03-19 09:26:18 +0100

    Removes distance description

--- handlers/public.js
+++ handlers/public.js
@@ -4,7 +4,6 @@ var message = {
   message: 'Hello, I\'m your API',
   departments: '/departments',
   department: '/department/<departmentId>',
-  distance: '/distance/<originAddress>/<destinationAddress>',
   journals: '/journals',
   journalsSearch: '/journals/<searchText>',
   journalsByDateDistinct: '/journals/date/distinct',
